28bda8815ff308f0236e67c623563d0f4f828a68
Milan Trbojevic <riverDeer12@gmail.com>
2021-09-16 14:04:23 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://moms-kitchen-api.azurewebsites.net/api',
+  apiUrl: 'api-url',
   captcha: {
-    siteKey: '6LdpLm8cAAAAAJ-CDiN59voXgHaBeAYjDizs2v2a',
-    secretKey: '6LdpLm8cAAAAAPtA5G4DSrXn3YZLSNAf4m8JIrxT',
+    siteKey: 'site-key',
+    secretKey: 'secret-key',
   }
 };

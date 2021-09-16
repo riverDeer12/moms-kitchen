2559713227da20ccd8167a7b49747a58832e70c9
Milan Trbojevic <mtrbojevic12@gmail.com>
2021-09-16 14:06:50 +0200

    ignore files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'api-url',
+  apiUrl: 'https://moms-kitchen-api.azurewebsites.net/api',
   captcha: {
-    siteKey: 'site-key',
-    secretKey: 'secret-key',
+    siteKey: '6LdpLm8cAAAAAJ-CDiN59voXgHaBeAYjDizs2v2a',
+    secretKey: '6LdpLm8cAAAAAPtA5G4DSrXn3YZLSNAf4m8JIrxT',
   }
 };
